465ffca9187a9601f638bd11cceda5bfdd8e96d1
James Bate <jbate2002@gmail.com>
2014-03-02 17:47:04 +0000

    Pass loading logic off to controller

--- routes/scorecard.js
+++ routes/scorecard.js
@@ -1,25 +1,11 @@
-exports.new = function(req, res){
-  res.render('scorecard/new', { title: 'Create new scorecard' });
-};
+var scorecard = require('../app/controllers/scorecard');
 
-exports.load = function(req, res){
-  // Load from "/scorecard/new" view
-  if(typeof req.param('homeTeam') != 'undefined' && typeof req.param('awayTeam') != 'undefined'){
-	res.redirect('/scorecard/' + req.param('homeTeam') + '/' + req.param('awayTeam'));
-  } 
-  // Load from RESTful URL
-  else {
-  	// Mock result (without database)
-    var innings = [
-      {name:"James", score: 65, balls: 97, fours: 8}, 
-      {name: "Jess", score: 20, howOut: "LBW", balls: 19, fours: 2, sixes: 1}, 
-      {name: "Frankie", score: 30, balls: 48}
-    ];
-    
-  	res.render('scorecard/view', { 
-        homeTeam: req.param('home'), 
-        awayTeam: req.param('away'), 
-        innings: innings 
-    });
-  }
-};
+/**
+ * Expose routes
+ */
+
+module.exports = function (app) {
+    app.get('/scorecard/new', scorecard.new);
+    app.post('/scorecard/new', scorecard.load);
+    app.get('/scorecard/:home/:away', scorecard.load);
+}
