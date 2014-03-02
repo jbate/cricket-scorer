ea18f186a33a6bb379305dad2c3431006af51a33
James Bate <jbate2002@gmail.com>
2014-03-02 17:47:47 +0000

    Load stuff from MVC architecture
    Outsource Express setup to its own config file too

--- app.js
+++ app.js
@@ -4,43 +4,48 @@
  */
 
 var express = require('express');
-var routes = require('./routes');
-var scorecard = require('./routes/scorecard');
 var http = require('http');
-var path = require('path');
+var fs = require('fs');
+var mongoose = require('mongoose');
 
 var app = express();
+// express settings
+require('./config/express')(app);
 
-// all environments
-app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(express.cookieParser('your secret here'));
-app.use(express.session());
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
-
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+// Bootstrap db connection
+// Connect to mongodb
+var connect = function () {
+  var location = 'mongodb://localhost:27017/cricket-scorer';
+  var options = { server: { socketOptions: { keepAlive: 1 } } }
+  mongoose.connect(location, options);
 }
+connect();
 
-// Main and login routes
-app.get('/', routes.index);
-app.get('/admin', routes.admin);
-app.get('/admin/:name', routes.admin)
-app.post('/login', routes.login);
+// Error handler
+mongoose.connection.on('error', function (err) {
+  console.log(err);
+})
 
-// Scorecard
-app.get('/scorecard/new', scorecard.new);
-app.post('/scorecard/new', scorecard.load);
-app.get('/scorecard/:home/:away', scorecard.load);
+// Reconnect when closed
+mongoose.connection.on('disconnected', function () {
+  connect();
+})
+
+// Bootstrap models
+var models_path = __dirname + '/app/models';
+fs.readdirSync(models_path).forEach(function (file) {
+  if (~file.indexOf('.js')) require(models_path + '/' + file)
+});
+
+
+// Bootstrap routes
+require('./routes/teams')(app);
+require('./routes/scorecard')(app);
+require('./routes/admin')(app);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
+
+// expose app
+exports = module.exports = app;
