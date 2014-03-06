010fd2828741133d66f069beee79616d702e4f47
James Bate <jbate2002@gmail.com>
2014-03-06 18:10:32 +0000

    Load and show player page

--- routes/player.js
+++ routes/player.js
@@ -5,8 +5,9 @@ var player = require('../app/controllers/player');
  */
 
 module.exports = function (app) {
-    //app.param('name', player.load)
-    //app.get('/player/:name', player.showPlayer);
+    app.param('playerId', player.load);
+
+    app.get('/player/:playerId', player.show);
     app.get('/player/new', player.createForm);
     app.post('/player/new', player.create);
 }
